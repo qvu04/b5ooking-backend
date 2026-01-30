4cbf6e5d8d4912bdded36c49d39dbbf61e7a523b
quangvungo <quangvu221004@gmail.com>
2026-01-30 18:06:27 +0700

    update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routers/index.js";
 import { errorHandler } from "./middleware/errorHandler.js";
-import {startBookingStatusCron } from "./cron/bookingStatus.cron.js";
+import { startBookingStatusCron } from "./cron/bookingStatus.cron.js";
 import { startActiveVoucherCron } from "./cron/startActiveVoucherCron.js";
 import { startStatusBooking } from "./cron/statusBooking.js";
 
@@ -16,9 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-startBookingStatusCron()
-startActiveVoucherCron()
-startStatusBooking()
+if (process.env.NODE_ENV === "production") {
+    startBookingStatusCron();
+    startActiveVoucherCron();
+    startStatusBooking();
+}
+
 app.get("/", (req, res) => {
     res.send("hello")
 })
